b12a0a9d5cc6783142a256452dc8d30e0998d31b
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2014-02-05 13:25:57 +0000

    Added ZV functionality for distributions

--- examples/rand_mvtdist.jl
+++ examples/rand_mvtdist.jl
@@ -1,4 +1,4 @@
-using Distributions, MCMC, ZVSimulator
+using Distributions, ZVSimulator
 
 function C(n::Int, c::Float64)
   X = eye(n)
@@ -9,11 +9,9 @@ end
 df, npars, a = 5., 4, 0.25
 c = ((df-2)/df)*C(npars, a)
 
-mcmodel = model(MvTDist(df, zeros(npars), c), init=zeros(npars))
-samplers = [HMCDA() for i in 1:5]
-tasks = mcmodel * samplers * SerialMC(steps=10000, burnin=1000)
+distributions = [MvTDist(df, zeros(npars), c) for i = 1:5]
 
-results = psim_serialmc_vrf(tasks)
+results = psim_rand_vrf(distributions)
 
 # writedlm_vrf(results, "./", ' ')
 # writedlm_reformat("_means.txt", "./_means_pprint.txt", fill("%.3e", 3))
--- run_tests.jl
+++ run_tests.jl
@@ -1,4 +1,4 @@
-my_tests = ["examples/mcmc_mvtdist.jl"]
+my_tests = ["examples/mcmc_mvtdist.jl", "examples/rand_mvtdist.jl", "examples/rand_tdist.jl"]
 
 println("Running tests:")
 
